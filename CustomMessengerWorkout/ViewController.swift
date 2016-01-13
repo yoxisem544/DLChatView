041f973c3a7adeff10b68b72a1d323c2e5e16a27
David <yoxisem544@gmail.com>
2016-01-13 10:50:45 +0800

    Initial Commit

---
+++ CustomMessengerWorkout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomMessengerWorkout
+//
+//  Created by David on 2016/1/13.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
